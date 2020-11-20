e183cb23b805d682f3dcca92f5d306eb0f1cc134
KaziPerens <perenskazi@mail.com>
2020-11-20 20:42:34 +0000

    Added comments, and polished code.

--- dendogram.py
+++ dendogram.py
@@ -1,40 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import scipy as sc
-
 from sklearn import cluster, datasets
-
 from sklearn.cluster import AgglomerativeClustering
-
 import numpy as np
-
 import matplotlib.pyplot as plt
-
 import pandas as pd
 
+# Import data
 train_features = pd.read_csv("train_features.csv") 
 
 test_features = pd.read_csv("test_features.csv") 
 
+# Merge train and test data, and remove the first 4 columns
 total_data = [train_features, test_features]
-
 total_data = pd.concat(total_data)#.head(n)
-
 total_data = np.array(total_data)
+total_data = total_data[:,4:]
 
-total_data = np.delete(total_data, 0, 1)
 
-total_data = np.delete(total_data, 0, 1)
-
-total_data = np.delete(total_data, 0, 1)
-
-total_data = np.delete(total_data, 0, 1)
-
+# Here, I used the agglemerative clustering
 cluster = AgglomerativeClustering(distance_threshold=np.inf, n_clusters=None, affinity='euclidean', linkage='average')
 cluster.fit_predict(total_data)      
 
@@ -42,6 +25,7 @@ link_M = cluster.children_
 
 n = len(total_data)
 
+# This function takes the linkage_Matrix, and creates a system of nested list that represent the hierarchy of the data.
 def expand(link_M, m, n):
     
     if m >= n:
@@ -54,6 +38,7 @@ def expand(link_M, m, n):
     return m
     
 dend = expand(link_M, len(link_M)+n-1, n)
+
 
 
 
